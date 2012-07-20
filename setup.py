2d0ce9d9eeba2f98fa7a7cbe7eb974d889e2cf60
Pig Monkey <pm@pig-monkey.com>
2012-07-20 11:38:14 -0700

    Fixing MANIFEST and template distribution

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'django-media',
     packages = find_packages(),
-    version = '1.0',
+    version = '1.0.1',
     description = 'A basic Django application to store and manage media.',
     author = 'Peter Hogg',
     author_email = 'peter@havenaut.net',
