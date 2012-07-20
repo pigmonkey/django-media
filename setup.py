dfd1ed880894097fd2398ba967b8e272c1871a9c
Pig Monkey <pm@pig-monkey.com>
2012-07-20 11:22:59 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'django-media',
     packages = find_packages(),
-    version = '0.1',
+    version = '1.0',
     description = 'A basic Django application to store and manage media.',
     author = 'Peter Hogg',
     author_email = 'peter@havenaut.net',
