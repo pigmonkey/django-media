c7f0bc857d9991dc79c991d24a57871833d650f6
Pig Monkey <pm@pig-monkey.com>
2012-08-02 15:10:50 -0700

    PEP8

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-media',
-    packages = find_packages(),
-    version = '1.0.1',
-    description = 'A basic Django application to store and manage media.',
-    author = 'Peter Hogg',
-    author_email = 'peter@havenaut.net',
-    url = 'https://github.com/pigmonkey/django-media',
-    classifiers = [
+    name='django-media',
+    packages=find_packages(),
+    version='1.0.1',
+    description='A basic Django application to store and manage media.',
+    author='Peter Hogg',
+    author_email='peter@havenaut.net',
+    url='https://github.com/pigmonkey/django-media',
+    classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
     ],
-    long_description = open('README.md').read(),
-    include_package_data = True,
+    long_description=open('README.md').read(),
+    include_package_data=True,
     zip_safe=False,
-    install_requires = ['PIL'],
+    install_requires=['PIL'],
 )
